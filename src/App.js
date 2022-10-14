99955f002eb709b3aa0c6f3e9ba9a512069df5ff
mohammed alkordy <114110945+M-Alkordy@users.noreply.github.com>
2022-10-14 14:08:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Fetch from './component/fetch/Fetch';
 function App() {
   return (
     <>
-    <Fetch id="" m="get" />
-    <Fetch id="" m="post" category="men's clothing" description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday" image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" price="109.95"/>
-    <Fetch id="7" m="put" category="men's clothing" description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday" image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" price="109.95"/>
-    <Fetch id="6" m="delete"/>
+    <Fetch id="" m="get"> Get</Fetch>
+    <Fetch id="" m="post" category="men's clothing" description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday" image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" price="109.95">Add </Fetch>
+    <Fetch id="7" m="put" category="men's clothing" description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday" image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" price="109.95"> update </Fetch>
+    <Fetch id="6" m="delete">delete </Fetch>
     </>
   );
 }
 
 export default App;
+
